1acb160a938928ff28b89bb05ebd7606b44406e4
dizzyfall <791700642@qq.com>
2024-01-15 22:45:08 +0800

    update 修改了生产环境变量

--- src/plugins/GlobalRequest.ts
+++ src/plugins/GlobalRequest.ts
@@ -12,6 +12,7 @@ import {message} from 'antd';
  */
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
+  prefix: process.env.NODE_ENV === 'production' ? 'http://www.dizzyfall.cn' : undefined
   // requestType: 'form',
 });
 
